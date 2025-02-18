ad297c92fb6263cb065b44b0930f056ec88577dc
OtiliaNicola <otilianicola94@gmail.com>
2025-02-18 11:26:52 +0100

    fix: add icons, structure html & scss

--- src/app/pages/tabs/pages/notifications/notifications.page.ts
+++ src/app/pages/tabs/pages/notifications/notifications.page.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons, IonCard, IonIcon, IonButton } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { alertCircleOutline, arrowBack, informationCircleOutline, warningOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.page.html',
   styleUrls: ['./notifications.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonCard, IonIcon, CommonModule, FormsModule]
 })
 export class NotificationsPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({
+      arrowBack,
+      warningOutline,
+      informationCircleOutline,
+      alertCircleOutline
+    })
+   }
 
   ngOnInit() {
   }
