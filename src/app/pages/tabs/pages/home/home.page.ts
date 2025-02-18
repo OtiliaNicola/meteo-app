237eb16beb6d0e0c87cc85eebdae1cb09a561724
OtiliaNicola <otilianicola94@gmail.com>
2025-02-18 11:27:40 +0100

    fix: add icons,  structure html & scss

--- src/app/pages/tabs/pages/home/home.page.ts
+++ src/app/pages/tabs/pages/home/home.page.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonTabButton, IonButtons, IonButton, IonCard, IonRow, IonGrid, IonCol, IonItem, IonImg, IonLabel, IonNote, IonCardContent } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { grid, reload, personOutline, waterOutline, speedometerOutline, partlySunnyOutline, addOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonImg, IonCol, IonGrid, IonRow, IonCard,IonGrid, IonButton, IonButtons, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({grid,reload,personOutline,waterOutline,speedometerOutline,partlySunnyOutline,addOutline});
+   }
 
   ngOnInit() {
   }
