a99bebac7cae555358fb8494179ef2cd5d22626c
OtiliaNicola <otilianicola94@gmail.com>
2025-02-18 11:21:37 +0100

    fix: addIcons in constructor, change structure html and css

--- src/app/pages/tabs/pages/search/search.page.ts
+++ src/app/pages/tabs/pages/search/search.page.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon, IonSearchbar, IonCard, IonInput, IonBackButton } from '@ionic/angular/standalone';
+import { arrowBack, searchCircle, grid, location, searchOutline, locationOutline, arrowBackOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonInput, IonButton,IonCard, IonButtons, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, IonInput, CommonModule, FormsModule]
 })
 export class SearchPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({arrowBackOutline,searchOutline,locationOutline,arrowBack,location,grid,searchCircle});
+   }
 
   ngOnInit() {
   }
