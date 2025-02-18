dc0e99075a4db2e471afeba5333c82778e59c63e
OtiliaNicola <otilianicola94@gmail.com>
2025-02-18 11:27:51 +0100

    fix: add icons,  structure html & scss

--- src/app/pages/tabs/pages/feedback/feedback.page.ts
+++ src/app/pages/tabs/pages/feedback/feedback.page.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonButton, IonButtons, IonBackButton, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { arrowBack } from 'ionicons/icons';
 
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.page.html',
   styleUrls: ['./feedback.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons, CommonModule, FormsModule]
 })
 export class FeedbackPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({
+      arrowBack
+    })
+   }
 
   ngOnInit() {
   }
