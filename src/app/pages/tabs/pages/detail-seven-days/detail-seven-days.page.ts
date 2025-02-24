cd2afe3488ceb0d9d6abbd512d99a17446348cc1
OtiliaNicola <otilianicola94@gmail.com>
2025-02-24 11:34:25 +0100

    feat: create view detail weather days

--- src/app/pages/tabs/pages/detail-seven-days/detail-seven-days.page.ts
+++ src/app/pages/tabs/pages/detail-seven-days/detail-seven-days.page.ts
@@ -1,20 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonIcon,
+  IonRow,
+  IonGrid,
+  IonImg,
+  IonCol,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { arrowBack } from 'ionicons/icons';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-detail-seven-days',
   templateUrl: './detail-seven-days.page.html',
   styleUrls: ['./detail-seven-days.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonCol,
+    IonImg,
+    IonGrid,
+    IonRow,
+    IonIcon,
+    IonButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    RouterModule,
+  ],
 })
 export class DetailSevenDaysPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({ arrowBack });
   }
 
+  ngOnInit() {}
 }
