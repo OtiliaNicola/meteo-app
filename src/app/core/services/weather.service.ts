8d8da10d27231a991c9260dac43b629e798ebdd5
OtiliaNicola <otilianicola94@gmail.com>
2025-02-24 11:35:03 +0100

    feat: create weather service

---
+++ src/app/core/services/weather.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { forkJoin, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(
+    private readonly httpClient: HttpClient,
+  ) { }
+
+  getWeatherByLatAndLon<T>(url: string, header:any): Observable<T>{
+    let params = new HttpParams()
+    .append('appid', environment.weatherApiKey)
+    Object.keys(header).forEach((key: string)=>{
+      params = params.append(key, header[key]);
+    })
+    return this.httpClient.get<T>(environment.baseUrl + url, {params});
+  }
+
+  getWeatherByCity<T>(city: string, header: any): Observable<T> {
+    let params = new HttpParams()
+      .append('appid', environment.weatherApiKey)
+      .append('q', city);  // Parámetro de ciudad
+    Object.keys(header).forEach((key: string) => {
+      params = params.append(key, header[key]);
+    });
+    return this.httpClient.get<T>(environment.baseUrl + 'weather', { params });
+  }
+
+  // Método para obtener el clima de varias ciudades a la vez
+  getWeatherForCities<T>(cities: string[], header: any): Observable<T[]> {
+    const cityRequests = cities.map((city) =>
+      this.getWeatherByCity<T>(city, header)
+    );
+    return forkJoin(cityRequests);  // Realiza las peticiones a todas las ciudades al mismo tiempo
+  }
+}
