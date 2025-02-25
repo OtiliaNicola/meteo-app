8d17e32af5deb143f2ab6cfd363ed6461686d9f1
OtiliaNicola <otilianicola94@gmail.com>
2025-02-25 13:10:18 +0100

    fix: add registerLocaleData

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,18 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import localeEs from '@angular/common/locales/es'; 
+import { registerLocaleData } from '@angular/common';
+import { LOCALE_ID } from '@angular/core';
+
+registerLocaleData(localeEs);
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideHttpClient()
+    provideHttpClient(),
+    { provide: LOCALE_ID, useValue: 'es-ES' }
   ],
 });
