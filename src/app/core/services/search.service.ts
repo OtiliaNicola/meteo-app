112d9b634d22d325bf52e8adad8bc7ad57e7f053
OtiliaNicola <otilianicola94@gmail.com>
2025-02-25 13:09:28 +0100

    fix: change getCitySuggestions

--- src/app/core/services/search.service.ts
+++ src/app/core/services/search.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { WeatherCity } from '../interfaces/weather-city.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -21,5 +22,15 @@ export class SearchService {
         });
       }
     return this.httpClient.get<any>(`${environment.baseUrl}weather`, { params });
+  }
+
+  getCitySuggestions(query: string): Observable<WeatherCity[]> {
+    let params = new HttpParams()
+      .set('appid', environment.weatherApiKey)
+      .set('lang', 'es')  // Configuramos el idioma como español
+      .set('q', query)    // Tomamos el texto que el usuario está escribiendo
+      .set('limit', '5'); // Limitar a las primeras 5 sugerencias
+
+    return this.httpClient.get<WeatherCity[]>(`${environment.baseGeoUrl}`, { params });
   }
 }
