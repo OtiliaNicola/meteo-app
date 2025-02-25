ab240c078bbcd4cc507c6b367593dc23996e925e
OtiliaNicola <otilianicola94@gmail.com>
2025-02-25 13:07:36 +0100

    fix: change navidatetosevendays

--- src/app/pages/tabs/pages/home/home.page.ts
+++ src/app/pages/tabs/pages/home/home.page.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import {
@@ -16,7 +16,7 @@ import {
   IonRow,
   IonTitle,
   IonToolbar,
-  ModalController
+  ModalController,
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import {
@@ -34,7 +34,6 @@ import { WeatherWeekend } from 'src/app/core/interfaces/weather-weekend.interfac
 import { GeolocationService } from 'src/app/core/services/geolocation.service';
 import { WeatherService } from 'src/app/core/services/weather.service';
 import { AddCityModalPage } from 'src/app/shared/add-city-modal/add-city-modal.page';
-
 
 @Component({
   selector: 'app-home',
@@ -58,22 +57,23 @@ import { AddCityModalPage } from 'src/app/shared/add-city-modal/add-city-modal.p
     FormsModule,
     HttpClientModule,
     RouterModule,
-    AddCityModalPage
- ],
- providers: [ModalController]
+    AddCityModalPage,
+  ],
+  providers: [ModalController],
 })
 export class HomePage implements OnInit {
   weatherTime!: WeatherData;
   forecastData!: WeatherWeekend;
   hourlyForecast!: WeatherHourly;
-  weatherData: WeatherData[] = []; 
-  cities: string[] = ['Madrid', 'Barcelona', 'Valencia', 'Sevilla'];
+  weatherData: WeatherData[] = [];
+  cities: string[] = [];
 
   constructor(
     private readonly geolocation: GeolocationService,
     private readonly weatherService: WeatherService,
     private readonly router: Router,
-    private readonly modalCtrl: ModalController
+    private readonly modalCtrl: ModalController,
+    private readonly changeDetectorRef: ChangeDetectorRef
   ) {
     addIcons({
       grid,
@@ -88,7 +88,11 @@ export class HomePage implements OnInit {
 
   ngOnInit() {
     this.getLocationUser();
-    this.getWeatherForMultipleCities();
+    const storedCities = localStorage.getItem('cities');
+    if (storedCities) {
+      this.cities = JSON.parse(storedCities).filter((city: string) => city && city.trim() !== ''); // Filtramos valores vacíos o nulos
+    }
+    this.getWeatherForMultipleCities(); // Cargar el clima para las ciudades almacenadas
   }
 
   async getLocationUser() {
@@ -99,7 +103,6 @@ export class HomePage implements OnInit {
       lat,
       units: 'metric',
       lang: 'sp',
-      cnt: '7',
     };
 
     this.weatherService
@@ -108,7 +111,7 @@ export class HomePage implements OnInit {
         data.main.temp = Math.round(data.main.temp);
         data.main.feels_like = Math.round(data.main.feels_like);
         data.main.temp_min = Math.round(data.main.temp_min);
-        data.main.temp_max = Math.round(data.main.temp_max);       
+        data.main.temp_max = Math.round(data.main.temp_max);
         this.weatherTime = data;
       });
 
@@ -126,26 +129,31 @@ export class HomePage implements OnInit {
             temp_max: Math.round(item.main.temp_max),
           },
         }));
-    
+
         this.hourlyForecast = data;
       });
   }
 
-  async openAddCityModal() {    
+  async openAddCityModal() {
     try {
       const modal = await this.modalCtrl.create({
         component: AddCityModalPage,
-        cssClass: 'my-custom-modal' // opcional, para estilos
+        cssClass: 'my-custom-modal', // opcional, para estilos
       });
-  
+
       await modal.present();
-  
+
       const result = await modal.onDidDismiss(); // Cambiado a onDidDismiss
-      console.log('Modal result:', result); // Para debug
-  
       if (result.data) {
-        this.cities.push(result.data);
-        await this.getWeatherForMultipleCities();
+        const newCity =  result.data.cityName.trim();
+        if (newCity && !this.cities.includes(newCity)) {
+          this.cities.push(newCity);
+          localStorage.setItem('cities', JSON.stringify(this.cities));
+          this.getWeatherForMultipleCities();
+          //this.changeDetectorRef.detectChanges();
+        }else {
+          console.log('La ciudad ya está en la lista o no es válida');
+        }
       }
     } catch (error) {
       console.error('Error en el modal:', error);
@@ -153,34 +161,47 @@ export class HomePage implements OnInit {
   }
 
   getWeatherForMultipleCities() {
-    
     const headers = {
       units: 'metric',
       lang: 'sp',
     };
+    const validCities = this.cities.filter((city: string) => city && city.trim() !== '');
+    if (validCities.length === 0) {
+      console.log('No hay ciudades válidas para mostrar el clima');
+      return;
+    }
+    if(this.cities.length > 0) {
+      // Obtener el clima de todas las ciudades al mismo tiempo
+      this.weatherService
+        .getWeatherForCities<WeatherData>(this.cities, headers)
+        .subscribe(
+          (data) => {
+            // Aquí puedes hacer cualquier modificación que necesites
+            // Ejemplo: redondear las temperaturas
+            data.forEach((cityWeather) => {
+              cityWeather.main.temp = Math.round(cityWeather.main.temp);
+              cityWeather.main.feels_like = Math.round(
+                cityWeather.main.feels_like
+              );
+              cityWeather.main.temp_min = Math.round(cityWeather.main.temp_min);
+              cityWeather.main.temp_max = Math.round(cityWeather.main.temp_max);
+            });
+            this.weatherData = data;
+          },
+          (error) => {
+            console.error('Error al obtener los datos del clima', error);
+          }
+        );
 
-    // Obtener el clima de todas las ciudades al mismo tiempo
-    this.weatherService.getWeatherForCities<WeatherData>(this.cities, headers).subscribe(
-      (data) => {
-        // Aquí puedes hacer cualquier modificación que necesites
-        // Ejemplo: redondear las temperaturas
-        data.forEach((cityWeather) => {
-          cityWeather.main.temp = Math.round(cityWeather.main.temp);
-          cityWeather.main.feels_like = Math.round(cityWeather.main.feels_like);
-          cityWeather.main.temp_min = Math.round(cityWeather.main.temp_min);
-          cityWeather.main.temp_max = Math.round(cityWeather.main.temp_max);
-        });
-        this.weatherData = data;
-      },
-      (error) => {
-        console.error('Error al obtener los datos del clima', error);
-      }
-    );
+    }
   }
 
   navigateToSevenDayForecast() {
     this.router.navigate(['/tabs/detail-seven-days'], {
-      state: { forecastData: this.forecastData }
+      state: {
+        weatherTime: this.weatherTime,
+        hourlyForecast: this.hourlyForecast,
+      },
     });
   }
 }
