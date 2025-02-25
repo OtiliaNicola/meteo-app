1ad1f9eb39a6a85d96c2d37295ced20320507758
OtiliaNicola <otilianicola94@gmail.com>
2025-02-25 13:09:57 +0100

    fix: delete spacing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,11 @@ import { provideRouter } from '@angular/router';
 import { provideIonicAngular } from '@ionic/angular/standalone';
 import { routes } from './app.routes';
 
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
     provideIonicAngular(),
-    
   ]
 };
