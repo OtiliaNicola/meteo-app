06d3d416dd384d853f9196c5d4f4027d1e5c1988
OtiliaNicola <otilianicola94@gmail.com>
2025-02-27 10:26:02 +0100

    fix: change structure & funcionality

--- src/app/pages/tabs/pages/feedback/feedback.page.ts
+++ src/app/pages/tabs/pages/feedback/feedback.page.ts
@@ -1,7 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { arrowBack } from 'ionicons/icons';
 import { UtilsService } from './../../../../core/services/utils.service';
@@ -11,25 +17,33 @@ import { UtilsService } from './../../../../core/services/utils.service';
   templateUrl: './feedback.page.html',
   styleUrls: ['./feedback.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonButton,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class FeedbackPage implements OnInit {
-  selectedCondition: string = ''; 
-  constructor(
-    private readonly utilsService: UtilsService
-  ) {
-    addIcons({arrowBack});
-   }
-
-  ngOnInit() {
+  selectedCondition: string = '';
+  constructor(private readonly utilsService: UtilsService) {
+    addIcons({ arrowBack });
   }
 
-  selectCondition(condition:string){
+  ngOnInit() {}
+
+  selectCondition(condition: string) {
     this.selectedCondition = condition;
   }
 
   submitFeedback() {
     // Lógica de la tarea
-    this.utilsService.presentToastSuccess('Gracias por su colaboración!', 'bottom');
+    this.utilsService.presentToastSuccess(
+      'Gracias por su colaboración!',
+      'bottom'
+    );
   }
 }
